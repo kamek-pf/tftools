f405b1815f60547756280c136f56d468385ea917
Kamek <b.kamek@gmail.com>
2020-04-10 01:14:27 -0400

    naive label map

--- src/pascal_voc/label_map.rs
+++ src/pascal_voc/label_map.rs
@@ -1,8 +1,18 @@
+use std::collections::HashMap;
+
 #[derive(Debug, Default)]
-pub struct LabelMap {}
+pub struct LabelMap {
+    map: HashMap<String, i64>,
+}
 
 impl LabelMap {
+    pub fn set(&mut self, label: String, value: i64) {
+        if !self.map.get(&label).is_none() {
+            self.map.insert(label, value);
+        }
+    }
+
     pub fn get(&self, label: &str) -> Option<i64> {
-        unimplemented!();
+        self.map.get(label).copied()
     }
 }
