f318617efb1ca202bd789e6d795d37ef86827b5f
Kamek <b.kamek@gmail.com>
2020-04-10 01:01:32 -0400

    fixed mess

--- src/pascal_voc/tfrecord.rs
+++ src/pascal_voc/tfrecord.rs
@@ -1,14 +1,20 @@
 use std::collections::HashMap;
-use std::fs;
+use std::fs::{self, OpenOptions};
+use std::io::{BufWriter, Error as IoError};
+use std::mem;
+use std::path::Path;
 
-use protobuf::RepeatedField;
+use protobuf::Message;
+use tensorflow::io::RecordWriter;
+use thiserror::Error;
 
 use super::label_map::LabelMap;
 use super::parser::Annotation;
 use crate::math;
 use crate::tensorflow_protos::example::Example;
-use crate::tensorflow_protos::feature::{BytesList, Feature, Features, FloatList, Int64List};
+use crate::tensorflow_protos::feature::{Feature, Features};
 
+/// Allows building tfrecord files by adding PASCAL VOC annotated examples
 #[derive(Debug, Default)]
 pub struct RecordBuilder {
     // Map labels to integers
@@ -16,27 +22,29 @@ pub struct RecordBuilder {
     // Max sized allowed for each output file
     max_size: usize,
     // Current estimate of the output file size
+    // @TODO: currently unused, update when record splitting is implemented
     current_size: usize,
     // Current chunk
+    // @TODO: currently unused, update when record splitting is implemented
     current_chunk: u64,
     // Examples that failed to load or are otherwise invalid
     ignored: Vec<String>,
-    // Features used by the neural net
-    features: Vec<BuilderFeatures>,
+    // Examples that should be part of the output tfrecord file
+    examples: Vec<ExampleImage>,
 }
 
+// Flat representation of an example
 #[derive(Debug, Default)]
-struct BuilderFeatures {
+struct ExampleImage {
     height: i64,
     width: i64,
     filename: String,
     image_bytes: Vec<u8>,
     image_format: String,
-
-    xmins: Vec<f64>, // List of normalized left x coordinates in bounding box (1 per box)
-    xmaxs: Vec<f64>, // List of normalized right x coordinates in bounding box # (1 per box)
-    ymins: Vec<f64>, // List of normalized top y coordinates in bounding box (1 per box)
-    ymaxs: Vec<f64>, // List of normalized bottom y coordinates in bounding box # (1 per box)
+    xmins: Vec<f32>, // List of normalized left x coordinates in bounding box (1 per box)
+    xmaxs: Vec<f32>, // List of normalized right x coordinates in bounding box # (1 per box)
+    ymins: Vec<f32>, // List of normalized top y coordinates in bounding box (1 per box)
+    ymaxs: Vec<f32>, // List of normalized bottom y coordinates in bounding box # (1 per box)
     classes: Vec<i64>, // List of integer class id of bounding box (1 per box)
     classes_text: Vec<String>, // List of string class name of bounding box (1 per box)
 }
@@ -51,6 +59,7 @@ impl RecordBuilder {
         }
     }
 
+    /// Add an example to the to the set
     pub fn add_example(&mut self, example: Annotation) {
         let ext = example
             .path
@@ -63,42 +72,63 @@ impl RecordBuilder {
 
         match (ext, fs::read(&example.path)) {
             (Some(ext), Ok(bytes)) => {
-                let width = example.size.width;
-                let height = example.size.height;
-                let features = &mut self.features;
-
-                // Map labels first, keep track of failures and bail
-                if let Some(mapped_labels) = map_labels(&example, &self.label_map) {
-                    features.classes.push(mapped_labels);
+                // First, map labels to their id and bail on error
+                let classes = if let Some(classes) = map_labels(&example, &self.label_map) {
+                    classes
                 } else {
                     self.ignored.push(example.path.to_string_lossy().into());
                     return;
-                }
+                };
 
-                // Add coordinates
-                let (xmins, xmaxs, ymins, ymaxs) = get_normalized_coordinates(&example);
-                features.xmins.push(xmins);
-                features.xmaxs.push(xmaxs);
-                features.ymins.push(ymins);
-                features.ymaxs.push(ymaxs);
-
-                // Add labels
-                features
-                    .classes_text
-                    .push(example.objects.iter().map(|o| o.name.clone()).collect());
-
-                // Add metadata and update recorder state
                 self.current_size += bytes.len();
-                features.height.push(height as i64);
-                features.height.push(width as i64);
-                features.filename.push(example.filename.clone());
-                features.image_bytes.push(bytes);
-                features.image_format.push(ext.to_owned());
+                let (xmins, xmaxs, ymins, ymaxs) = get_normalized_coordinates(&example);
+
+                let input = ExampleImage {
+                    height: example.size.height as i64,
+                    width: example.size.width as i64,
+                    filename: example.filename.clone(),
+                    image_bytes: bytes,
+                    image_format: ext.to_owned(),
+                    xmins,
+                    xmaxs,
+                    ymins,
+                    ymaxs,
+                    classes,
+                    classes_text: example.objects.iter().map(|o| o.name.clone()).collect(),
+                };
+
+                self.examples.push(input);
             }
             _ => {
                 self.ignored.push(example.path.to_string_lossy().into());
             }
         };
+    }
+
+    /// Write examples added to the builder to a tfrecord file
+    pub fn write_tfrecord(&mut self, path: &Path) -> Result<(), TfRecordError> {
+        let file = OpenOptions::new()
+            .write(true)
+            .create(true)
+            .truncate(true)
+            .open(path)?;
+
+        let buffered_writer = BufWriter::new(file);
+        let mut record_writer = RecordWriter::new(buffered_writer);
+
+        mem::take(&mut self.examples)
+            .into_iter()
+            .for_each(|example| {
+                let protobuf = Example::from(example);
+
+                // @TODO: better error reporting, look into Iterator's partition method
+                protobuf
+                    .write_to_bytes()
+                    .ok()
+                    .and_then(|bytes| record_writer.write_record(&bytes).ok());
+            });
+
+        Ok(())
     }
 }
 
@@ -111,8 +141,8 @@ fn map_labels(input: &Annotation, label_map: &LabelMap) -> Option<Vec<i64>> {
         .collect()
 }
 
-// Outputs vectors of normalized coordinates, tuple struct is (xmins, xmaxs, ymins, ymaxs)
-fn get_normalized_coordinates(input: &Annotation) -> (Vec<f64>, Vec<f64>, Vec<f64>, Vec<f64>) {
+// Outputs vectors of normalized coordinates, tuple structure is (xmins, xmaxs, ymins, ymaxs)
+fn get_normalized_coordinates(input: &Annotation) -> (Vec<f32>, Vec<f32>, Vec<f32>, Vec<f32>) {
     let labels_count = input.objects.len();
     let width = input.size.width;
     let height = input.size.height;
@@ -122,40 +152,48 @@ fn get_normalized_coordinates(input: &Annotation) -> (Vec<f64>, Vec<f64>, Vec<f6
     let mut ymaxs = Vec::with_capacity(labels_count);
 
     input.objects.iter().for_each(|object| {
-        xmins.push(math::normalize(object.bndbox.xmin, 0, width));
-        xmaxs.push(math::normalize(object.bndbox.xmax, 0, width));
-        ymins.push(math::normalize(object.bndbox.ymin, 0, height));
-        ymaxs.push(math::normalize(object.bndbox.ymax, 0, height));
+        xmins.push(math::normalize(object.bndbox.xmin, 0, width) as f32);
+        xmaxs.push(math::normalize(object.bndbox.xmax, 0, width) as f32);
+        ymins.push(math::normalize(object.bndbox.ymin, 0, height) as f32);
+        ymaxs.push(math::normalize(object.bndbox.ymax, 0, height) as f32);
     });
 
     (xmins, xmaxs, ymins, ymaxs)
 }
 
-// Map our features to TensorFlow's Example, which can be serialized into a tfrecord file
-impl From<BuilderFeatures> for Example {
-    fn from(builder: BuilderFeatures) -> Example {
-        let mut example = Example::new();
+// Map our internal representation of an example to the generic version used by TensorFlow
+impl From<ExampleImage> for Example {
+    fn from(input: ExampleImage) -> Example {
+        let mut output = Example::new();
         let mut features = Features::new();
         let mut features_map = HashMap::new();
 
-        insert_feature(&mut features_map, "image/height", builder.height);
-        insert_feature(&mut features_map, "image/width", builder.width);
+        insert_feature(&mut features_map, "image/height", input.height);
+        insert_feature(&mut features_map, "image/width", input.width);
 
         // According to the docs, "image/filename" and "image/source_id"
         // are both based on file name, see python sample code:
         // https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/using_your_own_dataset.md
-        let source_id = builder.filename.clone();
-        insert_feature(&mut features_map, "image/filename", builder.filename);
+        let source_id = input.filename.clone();
+        insert_feature(&mut features_map, "image/filename", input.filename);
         insert_feature(&mut features_map, "image/source_id", source_id);
-
-        insert_feature(&mut features_map, "image/encoded", builder.image_bytes);
-        insert_feature(&mut features_map, "image/format", builder.image_format);
-        insert_feature(&mut features_map, "image/object/bbox/xmin", builder.xmins);
+        insert_feature(&mut features_map, "image/encoded", input.image_bytes);
+        insert_feature(&mut features_map, "image/format", input.image_format);
+        insert_feature(&mut features_map, "image/object/bbox/xmin", input.xmins);
+        insert_feature(&mut features_map, "image/object/bbox/xmax", input.xmaxs);
+        insert_feature(&mut features_map, "image/object/bbox/ymin", input.ymins);
+        insert_feature(&mut features_map, "image/object/bbox/ymax", input.ymaxs);
+        insert_feature(&mut features_map, "image/object/class/text", input.classes);
+        insert_feature(
+            &mut features_map,
+            "image/object/class/label",
+            input.classes_text,
+        );
 
         features.set_feature(features_map);
-        example.set_features(features);
+        output.set_features(features);
 
-        example
+        output
     }
 }
 
@@ -165,72 +203,9 @@ fn insert_feature<V: Into<Feature>>(map: &mut HashMap<String, Feature>, attr: &s
     map.insert(attr, values.into());
 }
 
-impl From<Vec<i64>> for Feature {
-    fn from(input: Vec<i64>) -> Feature {
-        let mut values = Feature::new();
-        let mut list = Int64List::new();
-
-        list.set_value(input);
-        values.set_int64_list(list);
-
-        values
-    }
+/// Error types you might encounter while working with tfrecord files
+#[derive(Debug, Error)]
+pub enum TfRecordError {
+    #[error("Io error while attempting to write tfrecord file")]
+    Io(#[from] IoError),
 }
-
-impl From<Vec<f32>> for Feature {
-    fn from(input: Vec<f32>) -> Feature {
-        let mut values = Feature::new();
-        let mut list = FloatList::new();
-
-        list.set_value(input);
-        values.set_float_list(list);
-
-        values
-    }
-}
-
-impl From<Vec<String>> for Feature {
-    fn from(input: Vec<String>) -> Feature {
-        let mut values = Feature::new();
-        let mut list = BytesList::new();
-        let mut repeated = RepeatedField::new();
-
-        input.into_iter().for_each(|string| {
-            repeated.push(string.into_bytes());
-        });
-
-        list.set_value(repeated);
-        values.set_bytes_list(list);
-
-        values
-    }
-}
-
-impl From<Vec<Vec<u8>>> for Feature {
-    fn from(input: Vec<Vec<u8>>) -> Feature {
-        let mut values = Feature::new();
-        let mut list = BytesList::new();
-        let mut repeated = RepeatedField::new();
-
-        input.into_iter().for_each(|bytes| {
-            repeated.push(bytes);
-        });
-
-        list.set_value(repeated);
-        values.set_bytes_list(list);
-
-        values
-    }
-}
-
-// Structure: example <- features <- feature
-
-// @TODO:
-// - Push VOC into record builder
-// - If estimate > max_size:
-//      - Build Example from RecordBuilder
-//      - Serialize Example as protobuf text
-//      - Write to file with extension based on current_chunk
-//      - Update state and keep going
-// - If finish method is called:
-//      - current_chunk > 0 ? write file with extension : write file without extension
